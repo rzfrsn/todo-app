060093470c3010c05ef837786082abae7906b312
Tafita Raza <tafita.raza@outlook.com>
2022-12-26 14:22:46 +0300

    update main.jsx and index.scss

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import LoginPage from './pages/LoginPage'
+import LoginPage from './pages/LoginPage';
+import TodoPage from './pages/TodoPage';
 import './index.scss'
 
 const router = createBrowserRouter([
   {
     path: '/',
+    element: <TodoPage/>
+  },
+  {
+    path: '/login',
     element: <LoginPage/>
   }
 ])
