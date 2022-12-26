1a800ed0cc192e806f57794bbf47db603294cc26
Tafita Raza <tafita.raza@outlook.com>
2022-12-26 14:19:53 +0300

    fake tasks data

---
+++ src/data/fakeTasks.js
@@ -0,0 +1,22 @@
+export default [
+   {
+    id: 'task-1',
+    name: 'Finir la maquette de ToDo App sur figma',
+    isDone: false
+   },
+   {
+    id: 'task-2',
+    name: 'Apprendre Redux',
+    isDone: false
+   },
+   {
+    id: 'task-3',
+    name: 'Apprendre Firebase',
+    isDone: false
+   },
+   {
+    id: 'task-4',
+    name: 'Apprendre Figma',
+    isDone: false
+   },
+]
